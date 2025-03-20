a312551935dad85175ae5d9cc47b5dd1935e20bf
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2025-03-20 22:56:57 +0300

    Innit

---
+++ custom.js
@@ -0,0 +1,14 @@
+// This is needed to make it mobile responsive
+
+function adjustLayoutGroupsHeight() {
+  const widgetsHeight = document.getElementById(
+    "information-widgets"
+  ).offsetHeight;
+  const layoutGroups = document.getElementById("layout-groups");
+
+  const availableHeight = widgetsHeight + 10;
+  layoutGroups.style.marginTop = availableHeight + "px";
+}
+
+window.addEventListener("resize", adjustLayoutGroupsHeight);
+window.addEventListener("load", adjustLayoutGroupsHeight);
